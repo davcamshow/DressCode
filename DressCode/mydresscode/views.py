c7b5abe9188869a6884bed765db27678590efe48
Yuri Tec <l23120480@morelia.tecnm.mx>
2025-10-07 18:51:19 -0600

    Actalizaciones de la pantalla de recuperacion de contraseña y cuenta

--- DressCode/mydresscode/views.py
+++ DressCode/mydresscode/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 
-from django.shortcuts import render
-
 def home(request):
     return render(request, 'welcome.html')  
 
 def login_view(request):
     return render(request, 'login.html')  
+
+def recovery_view(request):
+    return render(request, 'recovery.html')
+
+def newPassword_view(request):
+    return render(request, 'newPassword.html')    
