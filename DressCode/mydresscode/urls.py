b63bf20f1f3da38788cb6b740527fcce3f25cddb
Yuri Tec <l23120480@morelia.tecnm.mx>
2025-10-09 22:59:25 -0600

    Responsivas de login y resto

--- DressCode/mydresscode/urls.py
+++ DressCode/mydresscode/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('recovery/', views.recovery_view, name='recovery'),
     path('newPassword/', views.newPassword_view, name='newPassword'),
+    path('register/', views.register_view, name='register'), 
 ]
