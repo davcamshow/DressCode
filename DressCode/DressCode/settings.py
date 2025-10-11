2a8e5b6fc2cda2dab7902eebc28864893cf119b3
Yuri Tec <l23120480@morelia.tecnm.mx>
2025-10-10 19:52:32 -0600

    actualizacion del front login arreglado

--- DressCode/DressCode/settings.py
+++ DressCode/DressCode/settings.py
@@ -55,7 +55,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':'dresscode',
         'USER':'postgres',
-        'PASSWORD':'postgres123',
+        'PASSWORD':'yuri12123',
         'HOST':'localhost',
         'PORT':'5432',
     }
