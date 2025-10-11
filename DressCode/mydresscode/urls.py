6e2b1b61a29b4b812ebc68594cb0e0dab0e00405
MarbellaItzel <l23120523@morelia.tecnm.mx>
2025-10-10 18:55:33 -0600

    Actualizacion

--- DressCode/mydresscode/urls.py
+++ DressCode/mydresscode/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('recovery/', views.recovery_view, name='recovery'),
     path('newPassword/', views.newPassword_view, name='newPassword'),
     path('register/', views.register_view, name='register'), 
+    path('register/password/', views.register_password_view, name='register_password'),
+
 ]
