26d3267fce88abd10dd1e1af72f4451a7f37f4e3
davcamshow <davidbarrerareyes98@gmail.com>
2025-10-11 15:16:22 -0600

    modified:   DressCode/DressCode/settings.py 	modified:   DressCode/mydresscode/templates/Cuenta creada.html 	modified:   DressCode/mydresscode/templates/Password.html 	modified:   DressCode/mydresscode/urls.py 	modified:   DressCode/mydresscode/views.py

--- DressCode/mydresscode/urls.py
+++ DressCode/mydresscode/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('newPassword/', views.newPassword_view, name='newPassword'),
     path('register/', views.register_view, name='register'), 
     path('register/password/', views.register_password_view, name='register_password'),
+    path('password/Cuenta creada', views.exit_view, name='Cuenta creada'),
     path('inicio/', views.inicio, name='inicio'),
     path('logout/', views.logout_view, name='logout'),
 ]
