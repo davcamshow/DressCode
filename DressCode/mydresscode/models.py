56144646fa584b15fa2769b75eb8468a2a8d2ffc
MarbellaItzel <l23120523@morelia.tecnm.mx>
2025-10-16 17:53:47 -0600

    actualizacion con tabla prenda

--- DressCode/mydresscode/models.py
+++ DressCode/mydresscode/models.py
@@ -11,4 +11,30 @@ class Usuario(models.Model):
         return self.email
 
     class Meta:
-        db_table = 'usuario'  
+        db_table = 'usuario'
+
+
+class Prenda(models.Model):
+    idPrenda = models.AutoField(primary_key=True)
+
+    idUsuario = models.ForeignKey(
+        'Usuario',              # también puedes usar Usuario directamente si está arriba
+        on_delete=models.CASCADE,
+        related_name='prendas',  # acceso: usuario.prendas.all()
+        db_column='idUsuario',   # nombre real de la columna en la BD
+    )
+
+    color = models.CharField(max_length=40)
+    tipo = models.CharField(max_length=50)        # camisa, falda, pantalón, etc.
+    imagen = models.CharField(max_length=255)     # ruta o URL en Supabase Storage
+    temporada = models.CharField(max_length=40, blank=True)
+    estilo = models.CharField(max_length=60, blank=True)
+    esFavorito = models.BooleanField(default=False)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'prenda'
+        ordering = ['-fecha']  # más recientes primero
+
+    def __str__(self):
+        return f"{self.tipo} ({self.color})"
