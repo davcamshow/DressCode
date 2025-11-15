9a37a137039fb734415bc6880c97fbb46f29155c
Maria <ortizmerinomaria7@gmail.com>
2025-11-15 14:39:50 -0600

    funcional  models y tabla de perfil en supabase

--- DressCode/mydresscode/models.py
+++ DressCode/mydresscode/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Usuario(models.Model):
@@ -105,3 +107,45 @@ class Recomendacion(models.Model):
 
     def __str__(self):
         return self.fecha_generacion
+    
+    #agrego para el perfil del usuario
+class Profile(models.Model):
+    user = models.OneToOneField(
+        'Usuario', 
+        on_delete=models.CASCADE,
+        primary_key=True,  
+        db_column='id_Usuario'  
+    )
+
+    nombre_completo = models.CharField(max_length=100, blank=True, null=True, db_column='nombre_completo') 
+    talla_superior = models.CharField(max_length=20, blank=True, null=True)  
+    talla_inferior = models.CharField(max_length=20, blank=True, null=True)  
+    talla_calzado = models.CharField(max_length=20, blank=True, null=True)  
+    estilos = models.JSONField(default=list, blank=True, db_column='estilos_fav')           
+    colores_fav = models.JSONField(default=list, blank=True)                               
+    temporadas_fav = models.JSONField(default=list, blank=True, db_column='temporadas_fav') 
+    config_completada = models.BooleanField(default=False, db_column='config_completada')      
+    creado_en = models.DateTimeField(auto_now_add=True)                                    
+    actualizado_en = models.DateTimeField(auto_now=True, db_column='actualizado_en')       
+    
+    class Meta:
+        db_table = 'perfil_usuario'
+        verbose_name = 'Perfil de usuario'
+        verbose_name_plural = 'Perfiles de usuarios'
+    
+    def __str__(self):
+        return f"Perfil de {self.user.nombre}"
+    
+    def agregar_estilo(self, estilo):
+        if estilo not in self.estilos:
+            self.estilos.append(estilo)
+            self.save()
+    
+    def agregar_color(self, color):
+        if color not in self.colores_fav:
+            self.colores_fav.append(color)
+            self.save()
+    
+    def completar_onboarding(self):
+        self.config_completada = True 
+        self.save()
