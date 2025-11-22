218583757e14dbe4f9e67167c58a15a478eebf1d
Maria <ortizmerinomaria7@gmail.com>
2025-11-22 12:47:40 -0600

    settings

--- DressCode/DressCode/settings.py
+++ DressCode/DressCode/settings.py
@@ -57,8 +57,15 @@ WSGI_APPLICATION = 'DressCode.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': os.getenv('SUPABASE_DB_PASSWORD'),
+        'HOST': os.getenv('SUPABASE_DB_HOST'),
+        'PORT': '5432',
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
