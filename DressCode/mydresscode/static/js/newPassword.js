6da14db02b7425319973b1d345b348a60dda719e
Yuri Tec <l23120480@morelia.tecnm.mx>
2025-11-28 19:31:48 -0600

    actualizacion

--- DressCode/mydresscode/static/js/newPassword.js
+++ DressCode/mydresscode/static/js/newPassword.js
@@ -1,3 +1,46 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("theme-toggle");
+
+  // Al cargar la página, revisa si hay un tema guardado en localStorage
+  const savedTheme = localStorage.getItem("theme") || "light";
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+  }
+
+  // Mostrar el ícono correcto al cargar
+  updateIcons(savedTheme);
+
+  // Evento al hacer clic en el botón
+  toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // Detectar nuevo tema
+    const isDark = document.body.classList.contains("dark-mode");
+    const newTheme = isDark ? "dark" : "light";
+
+    // Guardar preferencia
+    localStorage.setItem("theme", newTheme);
+
+    // Actualizar íconos
+    updateIcons(newTheme); });
+    
+  // Función para mostrar/ocultar íconos
+  function updateIcons(theme) {
+    const moonIcon = toggle.querySelector(".icon-moon");
+    const sunIcon = toggle.querySelector(".icon-sun");
+
+    if (theme === "dark") {
+      moonIcon.style.display = "none";
+      sunIcon.style.display = "inline-flex";
+    } else {
+      moonIcon.style.display = "inline-flex";
+      sunIcon.style.display = "none";
+    }
+  }
+
+});
+
 const passwordInput = document.getElementById("password");
 const reqLength = document.getElementById("req-length");
 const reqUppercase = document.getElementById("req-uppercase");
@@ -15,10 +58,7 @@ passwordInput.addEventListener("input", function () {
   );
 });
 
-const toggle = document.getElementById("theme-toggle");
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("dark-mode");
-});
+
 // Inicializar fondo dinámico
 function initDynamicBackground() {
   const canvas = document.getElementById("dynamic-bg");
